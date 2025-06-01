0ac3b5cc656571d82a2c6a7d5f3769b748c226bd
SheryMo <50407253+SheryMo@users.noreply.github.com>
2025-06-01 13:12:41 +0800

    Update ksplit-setup.sh

--- ksplit-setup.sh
+++ ksplit-setup.sh
@@ -1,50 +1,32 @@
 #!/bin/bash
+#
+# LLM environment initialization script
+# Author: <Your Name or Email>
+#
+# This script sets up a Python/Conda environment for LLM workloads, installs
+# required packages, and downloads the model from Hugging Face.
+#
+MOUNT_DIR=/local/rrMo
+LOGF="llm_setup.log"
+export HOME=/local/home/rrMo
+mkdir -p $HOME
+# Clone MoE_Update repository
+echo "==> Cloning MoE_Update repository..." | tee -a $LOGF
+git clone https://github.com/SheryMo/MoE_Update.git
 
-MOUNT_DIR=/opt/ksplit
-LOG_FILE=${HOME}/ksplit-setup.log
-LLVM_VERSION=10
-
-USER=${SUDO_USER}
-
-if [[ ${USER} == "" ]]; then
-  USER=$(id -u -n)
-fi
-
-if [[ ${SUDO_GID} == "" ]]; then
-  GROUP=$(id -g -n)
-else
-  GROUP=$(getent group  | grep ${SUDO_GID} | cut -d':' -f1)
-fi
-
-record_log() {
-  echo "[$(date)] $1" >> ${LOG_FILE}
+function update_system()
+{
+    echo "==> Updating system..." | tee -a $LOGF
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install -y python3-pip wget git
 }
 
-# Install llvm-10 from apt.llvm.org
-install_llvm() {
-  if [ $(clang --version | grep -o "version [0-9\.]\+" | awk '{print $2}') != "10.0.1" ]; then
-    record_log "Downloading llvm script to ${HOME}/llvm.sh"
-    wget https://apt.llvm.org/llvm.sh -O ${HOME}/llvm.sh
-    chmod +x ${HOME}/llvm.sh
-    sudo ${HOME}/llvm.sh ${LLVM_VERSION}
-    sudo ./update-alternatives-clang.sh ${LLVM_VERSION} 200
-  fi
-}
-
-install_dependencies() {
-  record_log "Begin setup!"
-  record_log "Installing dependencies..."
-  sudo apt update
-  sudo apt install -y build-essential nasm cmake libelf-dev libncurses5-dev gawk linux-headers-$(uname -r)
-  install_llvm
-}
-
-create_extfs() {
+function create_extfs() {
   record_log "Creating ext4 filesystem on /dev/sda4"
   sudo mkfs.ext4 -Fq /dev/sda4
 }
 
-mountfs() {
+function mountfs() {
   sudo mkdir ${MOUNT_DIR}
   sudo mount -t ext4 /dev/sda4 ${MOUNT_DIR}
 
@@ -74,160 +56,83 @@ prepare_local_partition() {
   mountfs
 }
 
-prepare_machine() {
-  install_dependencies
-  prepare_local_partition
+function install_miniconda()
+{
+    echo "==> Installing Miniconda..." | tee -a $LOGF
+    local INSTALLER="Anaconda3-2024.10-1-Linux-x86_64.sh"
+    sudo wget -c https://repo.anaconda.com/archive/$INSTALLER
+    bash $INSTALLER -b -p $HOME/anaconda3
+
+    export PATH=$HOME/anaconda3/bin:$PATH
+    eval "$(~/anaconda3/bin/conda shell.bash hook)"
+    conda init
 }
 
-# Clone all repos
-clone_pdg() {
-  if [ ! -d ${MOUNT_DIR}/pdg ]; then
-    record_log "Cloning PDG"
-    pushd ${MOUNT_DIR}
-    git clone https://github.com/ARISTODE/program-dependence-graph.git pdg --recursive --branch dev_ksplit
-    popd;
-  else
-    record_log "PDG dir not empty! skipping..."
-  fi
+function setup_conda_env()
+{
+    echo "==> Creating and activating conda environment 'silly_env'..." | tee -a $LOGF
+    conda create -y -n silly_env python=3.10
+    conda activate silly_env
 }
 
-clone_bareflank() {
-  if [ ! -d ${MOUNT_DIR}/bflank ]; then
-    mkdir -p ${MOUNT_DIR}/bflank;
-    record_log "Cloning Bareflank"
-    pushd ${MOUNT_DIR}/bflank
-    git clone https://github.com/mars-research/lvd-bflank.git bflank --depth 100 --branch dev_ksplit
-    mkdir cache build
-    popd;
-  else
-    record_log "Bareflank dir not empty! skipping..."
-  fi
+function install_dependencies()
+{
+    echo "==> Installing conda and pip dependencies..." | tee -a $LOGF
+    conda install -y cudatoolkit
+    pip install datasets==2.16.0 flask huggingface_hub netifaces seaborn
 }
 
-clone_linux() {  if [ ! -d ${MOUNT_DIR}/lvd-linux ]; then
-    record_log "Cloning LVD linux"
-    pushd ${MOUNT_DIR}
-    git clone https://github.com/mars-research/lvd-linux/ --branch dev_ksplit --depth 500 --recursive
-    popd;
-  else
-    record_log "lvd-linux dir not empty! skipping..."
-  fi
+function install_local_repository()
+{
+    local REPO_DIR="/local/repository"
+    echo "==> Installing local repository at $REPO_DIR..." | tee -a $LOGF
+
+    if [[ ! -d $REPO_DIR ]]; then
+        echo "ERROR: $REPO_DIR does not exist" | tee -a $LOGF
+        exit 1
+    fi
+
+    cd $REPO_DIR
+    pip install -e .
 }
 
-clone_bcfiles() {
-  if [ ! -d ${MOUNT_DIR}/bc-files ]; then
-    record_log "Cloning bc-files"
-    pushd ${MOUNT_DIR}
-    git clone https://gitlab.flux.utah.edu/xcap/bc-files.git --depth 1
-    popd;
-  else
-    record_log "bc-files dir not empty! skipping..."
-  fi
+function download_model()
+{
+    echo "==> Downloading model from Hugging Face..." | tee -a $LOGF
+
+    local MODEL_NAME="LLaMA-MoE-v1-3_5B-2_8"
+    local WORK_DIR="/local/repository/ll_test"
+    local DEST_DIR="/local/repository/llama-moe/$MODEL_NAME"
+
+    mkdir -p $WORK_DIR
+    cd $WORK_DIR
+
+    python3 <<EOF
+from huggingface_hub import snapshot_download
+snapshot_download(
+    repo_id="llama-moe/LLaMA-MoE-v1-3_5B-2_8",
+    local_dir="./",
+    local_dir_use_symlinks=False
+)
+EOF
+
+    mkdir -p $DEST_DIR
+    mv pytorch_model-00001-of-00002.bin $DEST_DIR/
+    mv pytorch_model-00002-of-00002.bin $DEST_DIR/
+    mv tokenizer.model $DEST_DIR/
 }
 
-clone_idlc() {
-  if [ ! -d ${MOUNT_DIR}/lcds-idl ]; then
-    record_log "Cloning lcds-idl"
-    pushd ${MOUNT_DIR}
-    git clone https://gitlab.flux.utah.edu/xcap/lcds-idl.git --branch feature-locks
-    popd;
-  else
-    record_log "lcds-idl dir not empty! skipping..."
-  fi
+function main()
+{
+    update_system
+    prepare_local_partition
+    install_miniconda
+    setup_conda_env
+    install_dependencies
+    # install_local_repository
+    download_model
+
+    echo "==> [$(date)] Setup complete. Activate with: conda activate silly_env" | tee -a $LOGF
 }
 
-clone_repos() {
-  clone_pdg;
-  clone_bareflank;
-  clone_linux;
-  clone_bcfiles;
-  clone_idlc;
-}
-
-## Build
-build_svf() {
-  record_log "Building SVF"
-  pushd ${MOUNT_DIR}/pdg/SVF
-  mkdir -p build && cd build;
-  cmake .. && make -j $(nproc)
-}
-
-build_pdg() {
-  record_log "Building PDG"
-  build_svf;
-  pushd ${MOUNT_DIR}/pdg
-  mkdir -p build && cd build;
-  cmake .. && make -j $(nproc)
-}
-
-build_bareflank(){
-  record_log "Building bareflank"
-  pushd ${MOUNT_DIR}/bflank/build
-  mv ../bflank/config.cmake ..
-  cmake ../bflank
-  make -j $(nproc)
-  popd;
-}
-
-build_module_init_tools(){
-  if [ x$(command -v lcd-insmod) == x"" ]; then
-    record_log "Building module init tools"
-    pushd lcd-domains/module-init-tools;
-    aclocal -I m4 && automake --add-missing --copy && autoconf
-    ./configure --prefix=/ --program-prefix=lcd-
-    make
-    sudo make install-exec
-    popd;
-  fi
-}
-
-install_linux() {
-  make -j $(nproc) modules
-  sudo make -j $(nproc) modules_install
-  sudo make -j $(nproc) headers_install
-  sudo make -j install
-}
-
-build_lcd_domains() {
-  record_log "Building lcd-domains"
-  pushd lcd-domains;
-  # do NOT use -j as it corrupts the .o files
-  make
-  popd;
-}
-
-build_linux() {
-  record_log "Building Linux"
-  pushd ${MOUNT_DIR}/lvd-linux;
-  cp config_lvd .config
-  make -j $(nproc)
-  if [ $(uname -r) != "4.8.4-lvd" ]; then
-    install_linux;
-  fi
-  build_module_init_tools
-  build_lcd_domains
-  popd;
-}
-
-build_idlc() {
-  record_log "Building idlc"
-  pushd ${MOUNT_DIR}/lcds-idl;
-  ./setup
-  mkdir -p build && cd build
-  cmake ..
-  make -j $(nproc)
-  ./idlc
-  popd
-}
-
-build_all() {
-  build_pdg;
-  build_bareflank;
-  build_linux;
-  build_idlc;
-}
-
-prepare_machine;
-clone_repos;
-build_all;
-record_log "Done Setting up!"
+main > $LOGF 2>&1
